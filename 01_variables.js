e85d2dd66e15d2f9e82c93ddc8e468db307a58f5
Shreshtha-ai <shreshtthaagarwal2007@gmail.com>
2026-02-07 23:45:30 +0530

    Created a program for learning  variables in js

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId = "123456"
+let accountEmail = "shreshtha@google.com"
+var accountPassword = "43354"
+accountcity = "Jaipur"
+
+// accountId = "234" not allowed because it is a constant variable
+accountEmail = "sag@outlook.com" 
+accountPassword = "54321"
+accountcity = "Delhi"
+let accountState;
+console.log(accountId) // undefined because it is not declared 
+
+/*
+not use var because of issue in block scope function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
