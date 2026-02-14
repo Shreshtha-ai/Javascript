651d8ef32c67ba8aa187d1bbfe94763be3f32602
Shreshtha <shreshthaagarwal2007@gmail.com>
2026-02-14 10:13:19 +0530

    Refactor comments and variable assignments in 01_variables.js
    Updated comments for clarity and corrected variable usage.

--- 01_variables.js
+++ 01_variables.js
@@ -3,7 +3,7 @@ let accountEmail = "shreshtha@google.com"
 var accountPassword = "43354"
 accountcity = "Jaipur"
 
-// accountId = "234" not allowed because it is a constant variable
+// accountId = "234" not allowed because it is a constant variable and cant be changed 
 accountEmail = "sag@outlook.com" 
 accountPassword = "54321"
 accountcity = "Delhi"
@@ -11,7 +11,7 @@ let accountState;
 console.log(accountId) // undefined because it is not declared 
 
 /*
-not use var because of issue in block scope function scope
+not use var because of issue in block scope and  function scope
 */
 
 console.table([accountId, accountEmail, accountPassword, accountcity, accountState])
