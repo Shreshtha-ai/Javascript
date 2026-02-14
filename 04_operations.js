132bfb77c93226d5e63af8be17f5ed479d076f03
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-02-14 22:20:42 +0530

    updated operations

---
+++ 04_operations.js
@@ -0,0 +1,31 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2); // modulus operator gives the remainder of the division
+console.log(2**3);
+
+let str1 ="Hello"
+let str2 = "SHRESHTHA"
+
+console.log(str1 +" "+  str2);
+
+console.log(1 +"2");
+console.log("1"+ 2);
+console.log("1"+ 2 + 2); // "1"+ 2 => "12" + 2 => "122"
+console.log(1 + 2 + "3"); // 1 + 2 => 3 + "3" => "33" because of left to right evaluation
+
+console.log(3 + 4 * 5 % 2) 
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter) 
+
+// Prefix and Postfix increment and decrement
+let x = 3
+console.log(x++) // 3 because it will return the value first and then increment
+console.log(x)
+
+let y = 3
+console.log(++y) // 4 because it will increment the value first and then return
+console.log(y) 
