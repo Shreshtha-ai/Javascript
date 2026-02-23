cfb6575e2269f06b0164c7ce92e0e2a079d46ab3
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-02-23 22:24:44 +0530

    added array manipulation examples and utility methods

--- 10_arrays.js
+++ 10_arrays.js
@@ -38,3 +38,32 @@ console.log("B", myArr);
 const myn2 = myArr.splice(2,3) // removes 3 elements starting from index 2
 console.log(myn2)
 console.log("C", myArr)
+
+const marvel_heroes = ['Ironaman','Spiderman','Daredevil']
+const dc_heroes = ['Batman','Superman','Flash']
+
+ marvel_heroes.push(dc_heroes) // marvel_heroes is now ['Ironaman','Spiderman','Daredevil',['Batman','Superman','Flash']]   
+console.log(marvel_heroes)
+
+console.log(marvel_heroes[3][1]) 
+
+const allHeroes = marvel_heroes.concat(dc_heroes) // concatenates two arrays
+console.log(allHeroes)
+
+const allHeroes2 = [...marvel_heroes,...dc_heroes] // spread operator to concatenate two arrays
+console.log(allHeroes2) 
+
+const array1 = [1,2,3,[4,5],6,7,[8,9,[12,23]]]
+
+const array2 = array1.flat(Infinity) // flattens the array to a single level
+console.log(array2)
+
+console.log(Array.isArray("Hello")) // checks if the value is an array
+console.log(Array.from("Hello"))
+console.log(Array.from({name: "Shreshtha"})) // since this is a object and we have not define if we want the key or the value to be converted to an array, it will return an empty array    
+
+let score = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score,score2,score3)) 
