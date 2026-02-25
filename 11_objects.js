3998d6b2265732c162a161792a86aa2ec26749c3
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-02-25 23:20:41 +0530

    added symbol key and updated object properties with methods

--- 11_objects.js
+++ 11_objects.js
@@ -3,8 +3,37 @@
 
 // object literal
 
+const mySym = Symbol("key1")
+
 const myObj = {
     name: "Shreshtha",
     "full name": "Shreshtha Agarwal",
+    [mySym]: "mykey1",
     age: 18,
+    location: "Jaipur",
+    email: "shreshtha@example.com",
+    isLoggedIn: true,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+   
 }
+console.log(myObj.name);
+console.log(myObj["email"]);
+console.log(myObj["full name"]) // we cant use dot notation when there is space in key name
+console.log(myObj[mySym]) // to access symbol key we have to use square bracket notation
+
+myObj.age = 19 // to update the value of a key
+// Object.freeze(myObj) // to make the object immutable
+myObj.age = 20 // this will not update the value of age as the object is frozen
+console.log(myObj.age); 
+
+myObj.greeting = function(){
+
+    console.log("Hello " + this.name);
+
+}
+
+myObj.greetingTwo = function(){
+    console.log(`Hello , ${this.name}`);
+}
+console.log(myObj.greeting());
+console.log(myObj.greetingTwo());
