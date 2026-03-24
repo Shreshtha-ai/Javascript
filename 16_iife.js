afdba1dae7a5186d484cfeb8d9f8da951d127302
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-03-24 21:24:45 +0530

    updated and added some comments related to iife

--- 16_iife.js
+++ 16_iife.js
@@ -1,10 +1,12 @@
 // Immediately Invoked Function Expression (IIFE)
 // is used to immediately execute a function after its defination and helps to avoid polluting the global scope with variables and functions
 
-(function(){
-    console.log("DB connected successfully");
+(function connection(){
+    console.log("DB connected successfully"); // named IIFE
 })(); // this will immediately execute the function and print the message in the console
 
 ((name) => {
-    console.log(`Welcome ${name} to our website`);
+    console.log(`Welcome ${name} to our website`); // unamed IIFE with parameter
 })("shreshtha")
+
+// when using two iife use ; to separate them to avoid syntax error
