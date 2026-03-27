7a91c187b68efeeeeedc9959e9f19d790e6d85c3
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-03-27 12:02:22 +0530

    added maps

---
+++ 26_map.js
@@ -0,0 +1,13 @@
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.map((num) => num + 10)
+
+console.log(newNums)
+
+//chaining of map and filter
+
+const chainedNums = myNums
+                        .map((num) => num + 10)
+                        .filter((num) => num > 15)
+
+console.log(chainedNums)
